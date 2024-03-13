54f07c88471559b7de6785cef091321188101423
chellinomx <162652132+chellinomx@users.noreply.github.com>
2024-03-13 13:10:23 -0500

    Update entrypoint.go

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -34,7 +34,7 @@ func myRoute(r *gin.RouterGroup) {
 
 	message := orderedmap.New()
 	message.Set("title", "Welcome to the Home Access Center API!")
-	message.Set("message", "This is the home page, visit the documentation at https://homeaccesscenterapi-docs.vercel.app/ for more information on how to use this API.")
+	message.Set("message", "This is the home page.")
 	message.Set("routes", []string{"/api/name", "/api/assignments", "/api/info", "/api/averages", "/api/classes", "/api/reportcard", "/api/ipr", "api/transcript", "api/rank"})
 
 	r.GET("/help", func(c *gin.Context) {
